21966676565c0bf6edfd0615aa94050ac1869033
Alhassan Tijani <jhaeti@gmail.com>
2023-07-03 05:59:31 +0000

    Added primary and secondary fonts

---
+++ frontend/src/app/fonts.js
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+export const primary = localFont({
+  src: "../../public/fonts/Avenir_LT_Std_55_Roman.otf",
+});
+
+export const secondary = localFont({
+  src: "../../public/fonts/OCR_A_Regular.ttf",
+});
