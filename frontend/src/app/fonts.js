71c55ab9967f14812a2d53f225af4c1b09157545
Alhassan Tijani <jhaeti@gmail.com>
2023-07-05 10:00:27 +0000

    Added bold font for primary font

--- frontend/src/app/fonts.js
+++ frontend/src/app/fonts.js
@@ -3,7 +3,11 @@ import localFont from "next/font/local";
 export const primary = localFont({
   src: "../../public/fonts/Avenir_LT_Std_55_Roman.otf",
 });
+export const primaryBold = localFont({
+  src: "../../public/fonts/Avenir_LT_Std_95_Black.otf",
+});
 
 export const secondary = localFont({
   src: "../../public/fonts/OCR_A_Regular.ttf",
+  weight: "normal",
 });
