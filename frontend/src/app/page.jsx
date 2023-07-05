7f7dcfa603f25a3fd6a05e1f477a1f445facd641
Alhassan Tijani <jhaeti@gmail.com>
2023-07-05 10:12:45 +0000

    Added hero and scroll down animation on hero

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,15 @@
-import Navbar from "./components/Navbar";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import ScrollDownAnimation from "./components/ScrollDownAnimation/ScrollDownAnimation";
 
 const Homepage = () => {
-  return <p>Hello from homepage</p>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ScrollDownAnimation />
+    </>
+  );
 };
 
 export default Homepage;
