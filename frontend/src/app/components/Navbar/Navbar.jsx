b9007df81361c23e388dffe96ff516360fc1dc2c
Alhassan Tijani <jhaeti@gmail.com>
2023-07-07 16:53:48 +0000

    Added logout functionality and choosing between light and dark logo

--- frontend/src/app/components/Navbar/Navbar.jsx
+++ frontend/src/app/components/Navbar/Navbar.jsx
@@ -1,34 +1,62 @@
 "use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
 import * as fonts from "../../fonts";
 import styles from "./styels.module.css";
 import useUserContext from "@/hooks/useUserContext";
+import { REMOVE_USER } from "@/context/UserContext";
+import apiUrl from "@/app/utils/apiUrl";
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const {
-    state: { isAuthenticated },
+    state: { isAuthenticated, token },
+    dispatch,
   } = useUserContext();
+
+  function logout() {
+    fetch(apiUrl + "/users/logout", {
+      credentials: "include",
+    }).then(() => {
+      dispatch({ type: REMOVE_USER });
+      router.push("/auth/login");
+    });
+  }
   return (
     <nav className={styles.nav}>
       <div className={"container" + " " + styles.container}>
         <Link href="/">
-          <Image src="/logo.svg" width={94} height={54} />
+          <Image
+            src={
+              pathname === "/" || pathname.startsWith("/auth")
+                ? "/logo.svg"
+                : "/logo_dark.svg"
+            }
+            width={94}
+            height={54}
+          />
         </Link>
         <ul>
           <li>
-            <Link href={isAuthenticated ? "/profile" : "/auth/login"}>
+            <Link
+              style={{
+                color:
+                  pathname === "/" || pathname.startsWith("/auth")
+                    ? "var(--color-white)"
+                    : "var(--color-dard)",
+              }}
+              href={isAuthenticated ? "/profile" : "/auth/login"}
+            >
               {isAuthenticated ? "Profile" : "Login"}
             </Link>
           </li>
           <li className={styles.join}>
             <button
               onClick={() => {
-                isAuthenticated
-                  ? console.log("Logout")
-                  : router.push("/auth/join");
+                isAuthenticated ? logout() : router.push("/auth/join");
               }}
               className={fonts.primaryBold.className + " " + styles.btn}
             >
