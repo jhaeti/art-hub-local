76fdf3482259a9d4fd6db5c5cf4b7dffc6bc12fa
Alhassan Tijani <jhaeti@gmail.com>
2023-07-09 05:55:46 +0000

    Fixed crash when image is not sent

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -12,12 +12,12 @@ const upload = multer({ limits: { fieldSize: 1000000 } });
 // Get all products from database
 // @return a [products]
 router.get("/products", async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.send(products);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+  try {
+    const products = await Product.find();
+    res.send(products);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 // Count number of Products in database
@@ -48,87 +48,89 @@ router.get("/products", async (req, res) => {
 // Post a product to /products
 // Access Should be a user and an Artist
 router.post(
-    "/products",
-    auth,
-    artistAuth,
-    upload.single("img"),
-    async (req, res) => {
-        const newProduct = new Product({
-            ...req.body,
-            img: req.file.buffer,
-            seller: req.user._id,
-            sellerName: req.user.name,
-        });
+  "/products",
+  auth,
+  artistAuth,
+  upload.single("img"),
+  async (req, res) => {
+    if (!req.file || !req.file.buffer)
+      return res.status(400).json("Please upload image");
+    const newProduct = new Product({
+      ...req.body,
+      img: req.file.buffer,
+      seller: req.user._id,
+      sellerName: req.user.name,
+    });
 
-        try {
-            const product = await newProduct.save();
-            res.status(201).json(product);
-        } catch (e) {
-            res.sendStatus(500);
-        }
+    try {
+      const product = await newProduct.save();
+      res.status(201).json(product);
+    } catch (e) {
+      res.sendStatus(500);
     }
+  }
 );
 
 // Deleting product
 // Access user and Artist who created that user
 router.delete("/products/:_id", auth, artistAuth, async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const product = await Product.findOneAndDelete({
-            _id,
-            seller: req.user._id,
-        });
-        // console.log(product);
-        if (!product) {
-            return res.sendStatus(400);
-        }
-        res.json(`Product with id: ${_id} is deleted successfully`);
-    } catch (e) {
-        res.sendStatus(500);
+  const { _id } = req.params;
+  try {
+    const product = await Product.findOneAndDelete({
+      _id,
+      seller: req.user._id,
+    });
+    // console.log(product);
+    if (!product) {
+      return res.sendStatus(400);
     }
+    res.json(`Product with id: ${_id} is deleted successfully`);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 // Getting product by id
 router.get("/products/:_id", async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const product = await Product.findById(_id);
-        res.send(product);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+  const { _id } = req.params;
+  try {
+    const product = await Product.findById(_id);
+    res.send(product);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 // Adding product to user favorite
 router.put("/products/add-to-fav/:_id", auth, async (req, res) => {
-    const { _id } = req.params;
+  const { _id } = req.params;
 
-    try {
-        if (!req.user.favorites.includes(_id))
-            req.user.favorites = [...req.user.favorites, _id];
-        req.user.save();
-        res.sendStatus(200);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+  try {
+    if (!req.user.favorites.includes(_id))
+      req.user.favorites = [...req.user.favorites, _id];
+    req.user.save();
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 // Removing product to user favorite
 router.put("/products/remove-from-fav/:_id", auth, async (req, res) => {
-    const { _id } = req.params;
+  const { _id } = req.params;
 
-    try {
-        if (req.user.favorites.includes(_id)) {
-            console.log("Before: ", req.user.favorites);
-            req.user.favorites = [...req.user.favorites].filter(
-                (item) => _id !== item.toString()
-            );
-            console.log("After: ", req.user.favorites);
-        }
-        req.user.save();
-        res.sendStatus(200);
-    } catch (e) {
-        res.sendStatus(500);
+  try {
+    if (req.user.favorites.includes(_id)) {
+      console.log("Before: ", req.user.favorites);
+      req.user.favorites = [...req.user.favorites].filter(
+        (item) => _id !== item.toString()
+      );
+      console.log("After: ", req.user.favorites);
     }
+    req.user.save();
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
