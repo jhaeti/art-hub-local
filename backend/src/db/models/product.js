7234050026c90647a2817f1c6e420cda6c5fece9
Alhassan Tijani <jhaeti@gmail.com>
2023-07-09 04:03:52 +0000

    Added sending buffer in image

--- backend/src/db/models/product.js
+++ backend/src/db/models/product.js
@@ -2,53 +2,53 @@ const mongoose = require("mongoose");
 
 // Creating Item Model
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-        trim: true,
-    },
-    quantity: {
-        type: Number,
-        required: true,
-    },
-    ns: {
-        type: Number,
-        default: 0,
-        alias: "numberSold",
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    img: {
-        type: Buffer,
-        required: true,
-    },
-    sellerName: { type: String, required: true },
-    seller: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  ns: {
+    type: Number,
+    default: 0,
+    alias: "numberSold",
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  img: {
+    type: Buffer,
+    required: true,
+  },
+  sellerName: { type: String, required: true },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 productSchema.methods.toJSON = function () {
-    const product = this;
+  const product = this;
 
-    const userObject = product.toObject();
-    delete userObject.img;
+  const userObject = product.toObject();
+  //   userObject.img = "data:image/jpeg;base64," + userObject.img;
 
-    return userObject;
+  return userObject;
 };
 
 const Product = mongoose.model("Product", productSchema);
