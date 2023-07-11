8a4cd98f82aa8610d85339283838b3b5a63da9a3
Alhassan Tijani <jhaeti@gmail.com>
2023-07-11 02:58:11 +0000

    Fetching and displaying cards in home screen

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,24 @@
+import { cookies } from "next/dist/client/components/headers";
 import Hero from "./components/Hero/Hero";
 import ScrollDownAnimation from "./components/ScrollDownAnimation/ScrollDownAnimation";
+import apiUrl from "./utils/apiUrl";
+import Cards from "./components/Cards/Cards";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const res = await fetch(apiUrl + "/products", {
+    headers: {
+      Cookie: cookies(),
+    },
+  });
+  const data = await res.json();
+
   return (
     <>
       <Hero />
       <ScrollDownAnimation />
+      <div className="container mt-6">
+        {res.ok && <Cards arts={data} showSellerName columns={3} />}
+      </div>
     </>
   );
 };
