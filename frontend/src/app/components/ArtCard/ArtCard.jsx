56c355cc51f2c0e3b1cd6231f8a24a11a3e7afb8
Alhassan Tijani <jhaeti@gmail.com>
2023-07-11 02:58:36 +0000

    Art card component

--- frontend/src/app/components/ArtCard/ArtCard.jsx
+++ frontend/src/app/components/ArtCard/ArtCard.jsx
@@ -1,5 +1,26 @@
-const ArtCard = () => {
-  return <div>Card</div>;
+"use client";
+
+import { useRouter } from "next/navigation";
+import RemoveArtBtn from "./RemoveArtBtn";
+import styles from "./styles.module.css";
+
+const ArtCard = ({ art, showSellerName, showRemoveBtn }) => {
+  const router = useRouter();
+  return (
+    <div className={styles.card}>
+      <img
+        onClick={() => {
+          router.push("/products/" + String(art._id));
+        }}
+        className={styles.img}
+        src={"data:image/jpg;base64," + art.img}
+        alt={art.description}
+      />
+      {showSellerName && <p> - {art.sellerName}</p>}
+
+      {showRemoveBtn && <RemoveArtBtn id={art._id} />}
+    </div>
+  );
 };
 
 export default ArtCard;
