efa8f8ee1c77fb3cf0fd2904ce91511998ee2b26
Alhassan Tijani <jhaeti@gmail.com>
2023-07-11 02:57:25 +0000

    Getting user data on the server side and updated the UserContext through Navbar

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,19 +1,29 @@
 import "./index.css";
 import { primary } from "./fonts";
+
 import Navbar from "./components/Navbar/Navbar";
 import Provider from "../context/Provider";
+import apiUrl from "./utils/apiUrl";
+import { cookies } from "next/dist/client/components/headers";
 
 export const metadata = {
   title: "Art Hub Local",
   description: "Website for making art and culture accessible",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const res = await fetch(apiUrl + "/users/me", {
+    headers: {
+      Cookie: cookies(),
+    },
+  });
+  const data = await res.json();
+
   return (
     <html lang="en">
       <body className={primary.className}>
         <Provider>
-          <Navbar />
+          <Navbar userData={res.ok && data} />
           {children}
         </Provider>
       </body>
