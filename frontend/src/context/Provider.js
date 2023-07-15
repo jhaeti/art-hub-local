7d4801d3746229288021c6d713d72ed0e7caae46
Alhassan Tijani <jhaeti@gmail.com>
2023-07-15 01:54:55 +0000

    Moved context and hooks into home

--- frontend/src/context/Provider.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import { UserContextProvider } from "./UserContext";
-
-const Provider = ({ children }) => {
-  return <UserContextProvider>{children}</UserContextProvider>;
-};
-
-export default Provider;
