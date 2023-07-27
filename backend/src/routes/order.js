45e4133a8bbd4035bf6c0dc99987b00e610fdc78
Alhassan Tijani <jhaeti@gmail.com>
2023-07-27 03:16:39 +0000

    Updated previous product after each order

--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -1,13 +1,29 @@
 const Order = require("../db/models/order");
+const Product = require("../db//models/product");
 const router = require("express").Router();
 
 const artistAuth = require("./middleware/artistAuth");
 const auth = require("./middleware/auth");
 
+// Router for making an order
 router.post("/products/order", auth, async (req, res) => {
   try {
-    const newOrder = new Order({ ...req.body, buyer: req.user._id });
-    const order = await newOrder.save();
+    const { amount, seller, productId, productName, price, quantity } =
+      req.body;
+    const product = await Product.findOne({ _id: productId });
+    product.ns = product.ns + quantity;
+    product.quantity = product.quantity - quantity;
+    const productAfterSave = await product.save();
+    const newOrder = new Order({
+      amount,
+      seller,
+      productId,
+      productName,
+      price,
+      quantity,
+      buyer: req.user._id,
+    });
+    const order = productAfterSave && (await newOrder.save());
     res.json(order);
   } catch (e) {
     res.sendStatus(500);
